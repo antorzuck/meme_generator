f44c19a5e7d5d810c73a7616f205cd5cb7c61fcd
Antor Mahmud <antorzuck@gmail.com>
2022-09-23 21:09:40 +0600

    project about to finished

--- base/views.py
+++ base/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from base.models import *
+from PIL import Image, ImageDraw, ImageFont
+import random
+import os
+from django.conf import settings
 
 def home(request):
-    return render(request, 'base.html')
+    context = {
+        'temp' : Template.objects.all()
+    }
+    return render(request, 'base.html', context)
 
+def selected(request, uid):
+    gi = Template.objects.get(uid=uid)
+    print(gi.image.url)
+
+    context = {
+        'temp' : Template.objects.all(),
+        'photo' : gi
+    }
+    return render(request, 'base.html', context)
+
+def generate(request):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        tid = request.POST.get('tempid')
+
+        get_temp = Template.objects.get(uid=tid)
+
+        img = Image.open(f'{settings.BASE_DIR}{get_temp.image.url}')
+        drew = ImageDraw.Draw(img)
+        font = ImageFont.truetype("comicbd.ttf", 50)
+        drew.text((50, 30), text, font=font, fill=(255,0,0))
+        name = random.randint(100000,999999)
+        img.save(f"{settings.BASE_DIR}/media/{name}.png")
+        return redirect('/')
